ee2e8a1234b2062e9ae578ef72c281d6e1c3dc5f
Nicholas Synovic <nicholas.synovic@gmail.com>
2022-05-31 15:51:27 -0500

    Update clean.bash

--- clean.bash
+++ clean.bash
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# This is a tool to clean up a Python file and generate reports
+
 filename=$(basename -- "$fullfile")
 filename="${filename%.*}"
 
-poetry run isort $1
+mkdir checks
+
+# Code formatters
 poetry run autoflake --in-place --remove-all-unused-imports --remove-duplicate-keys --remove-unused-variables $1
 poetry run black $1
+poetry run isort $1
 
-bandit -i --severity-level all -o bandit_$filename.txt $1
-mypy --strict --pretty --txt-report mypy_$filename.txt $1
+# Security
+poetry run bandit --severity-level all --confidence-level all --format txt -o checks/bandit_$1.txt $1
+
+# Static typing
+poetry run mypy $1 > checks/mypy_$1.txt
+
+# Linting
+rm checks/flake8_$1.txt
+poetry run flake8 --output-file checks/flake8_$1.txt --isolated $1
+poetry run pylint --rcfile pylintrc --output checks/pylint_$1.txt -f text $1
